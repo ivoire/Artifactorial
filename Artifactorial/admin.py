12c1ae00df22a73c15852549bc331eb86e7ec8f0
Rémi Duraffort <ivoire@videolan.org>
2014-07-29 15:49:53 +0200

    Improve the admin interface

--- Artifactorial/admin.py
+++ Artifactorial/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from Artifactorial.models import Artifact, Directory
 
 
-admin.site.register(Artifact)
-admin.site.register(Directory)
+class ArtifactAdmin(admin.ModelAdmin):
+    list_display = ('path', 'directory',)
+
+
+class DirectoryAdmin(admin.ModelAdmin):
+    list_display = ('path', 'user', 'group', 'is_public',)
+
+admin.site.register(Artifact, ArtifactAdmin)
+admin.site.register(Directory, DirectoryAdmin)
