546e0d3c0bf2f729e87691309c3cc3abc553ff6a
Rémi Duraffort <ivoire@videolan.org>
2014-07-31 09:44:10 +0200

    Remove unused imports

--- Artifactorial/urls.py
+++ Artifactorial/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
 
-from django.conf.urls import include, patterns, url
-from django.core.urlresolvers import reverse_lazy
+from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('Artifactorial.views',
                        url(r'^$', 'index', name='index'),
-                       url(r'^post', 'post', name='post'))
+                       url(r'^post', 'post', name='post'),
+                       url(r'^get(?P<filename>/.*)$', 'get', name='get'))
