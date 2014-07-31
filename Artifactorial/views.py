537abc08460333c02aac66319a077858954e6933
Rémi Duraffort <ivoire@videolan.org>
2014-07-31 10:59:05 +0200

    Add directory listing

--- Artifactorial/views.py
+++ Artifactorial/views.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
 
+from django.db.models import Q
 from django.core.exceptions import PermissionDenied
 from django.forms import ModelForm
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 
 from Artifactorial.models import Artifact, Directory
+
+import os
 
 
 def index(request):
@@ -43,3 +46,41 @@ def post(request):
             raise PermissionDenied
     else:
         raise PermissionDenied
+
+
+def get(request, filename):
+    data=''
+    # Is it a file or a path
+    if filename[-1] == '/':
+        dirname = os.path.dirname(filename)
+        directories = Directory.objects.filter(Q(path__startswith="%s" % (dirname)) | Q(path=dirname))
+        if not directories:
+            # TODO: Look for pseudo directories with files
+            raise Http404
+
+        dirname_length = len(dirname)
+        dir_set = set()
+        for directory in directories:
+            if directory.path != dirname:
+                # Sub directory => print the next elements in the path
+                full_dir_name = directory.path[dirname_length+1:]
+                try:
+                    index = full_dir_name.index('/')
+                    dir_set.add(full_dir_name[:index])
+                except Exception:
+                    dir_set.add(full_dir_name)
+
+        artifacts = Artifact.objects.filter(path__startswith=filename.lstrip('/'))
+        art_set = set()
+        for artifact in artifacts:
+            # TODO: handle pseudo directories
+            art_set.add(artifact.path.name[dirname_length:])
+
+        return render_to_response('Artifactorial/list.html',
+                                  {'directory': dirname, 'directories': dir_set,
+                                   'files': art_set},
+                                  context_instance=RequestContext(request))
+    else:
+        artifact = get_object_or_404(Artifact, path=filename.lstrip('/'))
+        data = "%s (%d)" % (artifact.path.name, artifact.path.size)
+        return HttpResponse(data)
