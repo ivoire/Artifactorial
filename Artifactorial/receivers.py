ef45e1d0358d387b5d83248410301c12d3178745
Rémi Duraffort <ivoire@videolan.org>
2014-08-01 10:33:19 +0200

    Really remove files when the Artifact is deleted

---
+++ Artifactorial/receivers.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from Artifactorial.models import Artifact
+
+
+@receiver(post_delete, sender=Artifact)
+def artifact_post_delete(sender, **kwargs):
+    artifact = kwargs['instance']
+    artifact.path.storage.delete(artifact.path.path)
