641f9307adaa09c7571952b6b8aec23ba48fd719
Rémi Duraffort <ivoire@videolan.org>
2014-08-01 15:24:04 +0200

    Fix landscape error

--- Artifactorial/management/commands/purge.py
+++ Artifactorial/management/commands/purge.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from Artifactorial.models import Directory
 
 from optparse import make_option
