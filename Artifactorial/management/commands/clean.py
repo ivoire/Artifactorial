5227217a0b1b99e23db5ab1c667a863410f05014
Rémi Duraffort <ivoire@videolan.org>
2014-08-06 13:48:41 +0200

    Remove unused import

--- Artifactorial/management/commands/clean.py
+++ Artifactorial/management/commands/clean.py
@@ -22,8 +22,6 @@ from __future__ import unicode_literals
 from django.core.management.base import BaseCommand
 from Artifactorial.models import Directory
 
-from optparse import make_option
-
 
 class Command(BaseCommand):
     args = None
