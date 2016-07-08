f4d217fd3078042751d3efddacf372177f63ec29
Rémi Duraffort <ivoire@videolan.org>
2016-07-08 10:25:43 +0200

    Upgrade management commands

--- Artifactorial/management/commands/purge.py
+++ Artifactorial/management/commands/purge.py
@@ -20,29 +20,19 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from Artifactorial import get_version
 from Artifactorial.models import Directory
-
-from optparse import make_option
 
 
 class Command(BaseCommand):
     args = None
     help = 'Purge old files'
-    option_list = BaseCommand.option_list + (
-        make_option('--ttl',
-                    dest='TTL',
-                    default=90,
-                    type=int,
-                    help='Time To Live in days'),
-        make_option('--all',
-                    dest='all',
-                    default=False,
-                    action="store_true",
-                    help='Also remove permanent files'))
 
-    def get_version(self):
-        return get_version()
+    def add_arguments(self, parser):
+        parser.add_argument('--ttl', dest='TTL', default=90, type=int,
+                            help='Time To Live in days')
+        parser.add_argument('--all', dest='all', default=False,
+                            action="store_true",
+                            help='Also remove permanent files')
 
     def handle(self, *args, **kwargs):
         for directory in Directory.objects.all():
