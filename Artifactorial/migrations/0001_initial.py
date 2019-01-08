b468a6ff70ad0e978d12f1e8fa63385520adbf8c
Rémi Duraffort <ivoire@videolan.org>
2019-01-08 13:18:05 +0100

    Fix migration with old Django version
    The exact migration might be missing.

--- Artifactorial/migrations/0001_initial.py
+++ Artifactorial/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0007_alter_validators_add_error_messages'),
+        ('auth', '__first__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
