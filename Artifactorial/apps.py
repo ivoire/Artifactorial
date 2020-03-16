ca4212e982115c35affa99c80d1a6fd15fd95a04
Rémi Duraffort <remi.duraffort@linaro.org>
2020-03-16 15:55:45 +0100

    Re-license to MIT

--- Artifactorial/apps.py
+++ Artifactorial/apps.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
-
-# Copyright 2016 Rémi Duraffort
-# This file is part of Artifactorial.
 #
-# Artifactorial is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright 2016-2017 Rémi Duraffort
+# Copyright 2017-present Linaro Limited
 #
-# Artifactorial is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# Author: Rémi Duraffort <remi.duraffort@linaro.org>
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with Artifactorial.  If not, see <http://www.gnu.org/licenses/>
+# SPDX-License-Identifier: MIT
 
 from django.apps import AppConfig
 
